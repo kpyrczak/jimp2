a50b9e51ff7019c2e4d088dd11112ab907d969d8
kpyrczak <48697456+kpyrczak@users.noreply.github.com>
2019-04-02 07:37:03 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,15 +1,10 @@
-//Definicja znajduje się w pliku Point.cpp
+
 #include <cmath>
 #include <iostream>
 #include "Point.h"
 using namespace std;
 
-/* Aby wskazać, ze definicja funkcji dotyczy metody danej klasy
-  stosujemy tzw. operator zasięgu - "::" 
-*/
 
-//Specjalna inicjalizacja zmiennych. Zmienne są inicjowane
-//nim zostanie wywołane ciało konstruktora
 Point::Point():x_(0),y_(0){
   cout << "Konstruktor bezparametrowy" << endl;
 }
