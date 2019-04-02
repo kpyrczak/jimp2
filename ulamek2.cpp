7f8911f341346fa09f9fa2a3f6fa7f2997d58644
kpyrczak <48697456+kpyrczak@users.noreply.github.com>
2019-04-02 07:39:22 +0200

    Update ulamek2.cpp

--- ulamek2.cpp
+++ ulamek2.cpp
@@ -6,29 +6,25 @@ using namespace std;
 class Ulamek{
 private:
 	int licznik, mianownik, id;
-	static int cnt; //pole statyczne zwiazane z cala klasa, 
+	static int cnt; 
 public:
 	Ulamek(): licznik(0), mianownik(1), id(++cnt){
 		cout<<"Ulamek(): "<<id<<endl;
 	}
-	 //konstruktor, niczego nie zwraca, bezparametrowy, lista inicjalizacyjna
+	 
 
 
 
 
 	Ulamek(int l, int m): licznik(l), mianownik(m), id(++cnt){
 		cout<<"Ulamek(int,int): "<<id<<endl;
-	} //konstruktor agregujacy
-		//inicjalizacja obiekut jest zgodna z koljenoscia umieszczzenia tych rzeczy w pamięci
-	
-
+	} 
 
 
 	Ulamek(const Ulamek &other): licznik(other.licznik), mianownik(other.mianownik), id(++cnt){
 		cout<<"Ulamek(const Ulamek $other): "<<id<<endl;
 	}
-		 //konstruktor kopiujący, jako parametr dostaje referencje do innego ułamka, który ma skopiować, ma do			step do prywatnych obiektow, bo jest zdefiniowany w tej klasie
-	
+		 
 	~Ulamek(){
 		cout<<"~Ulamek(): "<<id<<endl;
 	}
@@ -39,7 +35,7 @@ public:
 		buff << licznik << "/" << mianownik;
 		return buff.str();
 	}
-						//const, bo nie zmienia obiektu;	
+							
 };
 int Ulamek::cnt=0;
 
