0532c1a82ce1542fc58d603269b38c39e79900df
kpyrczak <48697456+kpyrczak@users.noreply.github.com>
2019-04-02 07:40:03 +0200

    Update ulamek.cpp

--- ulamek.cpp
+++ ulamek.cpp
@@ -8,23 +8,16 @@ using namespace std;
 Ulamek::Ulamek(): licznik(0), mianownik(1), id(++cnt){
 	cout<<"Ulamek(): "<<id<<endl;
 }
-//konstruktor, niczego nie zwraca, bezparametrowy, lista inicjalizacyjna
-
-
 
 
 Ulamek::Ulamek(int l, int m): licznik(l), mianownik(m), id(++cnt){
 	cout<<"Ulamek(int,int): "<<id<<endl;
-} //konstruktor agregujacy
-//inicjalizacja obiekut jest zgodna z koljenoscia umieszczzenia tych rzeczy w pamięci
-
-
+} 
 
 
 Ulamek::Ulamek(const Ulamek &other): licznik(other.licznik), mianownik(other.mianownik), id(++cnt){
 	cout<<"Ulamek(const Ulamek $other): "<<id<<endl;
 }
-//konstruktor kopiujący, jako parametr dostaje referencje do innego ułamka, który ma skopiować, ma do			step do prywatnych obiektow, bo jest zdefiniowany w tej klasie
 
 Ulamek::~Ulamek(){
 	cout<<"~Ulamek(): "<<id<<endl;
@@ -36,7 +29,7 @@ string Ulamek::toString()const{
 	buff << licznik << "/" << mianownik;
 	return buff.str();
 }
-//const, bo nie zmienia obiektu;	
+	
 
 int Ulamek::cnt=0;
 
