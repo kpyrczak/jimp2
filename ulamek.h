0956f9bc26274145072b9e7e38051478a0e91f10
kpyrczak <48697456+kpyrczak@users.noreply.github.com>
2019-04-09 01:22:43 +0200

    Update ulamek.h

--- ulamek.h
+++ ulamek.h
@@ -6,26 +6,19 @@ using namespace std;
 class Ulamek{
 private:
 	int licznik, mianownik, id;
-	static int cnt; //pole statyczne zwiazane z cala klasa, 
+	static int cnt; 
 public:
 	Ulamek();
-	 //konstruktor, niczego nie zwraca, bezparametrowy, lista inicjalizacyjna
-
-
-
+	 
 
 	Ulamek(int l, int m);
-	//konstruktor agregujacy
-		//inicjalizacja obiekut jest zgodna z koljenoscia umieszczzenia tych rzeczy w pamięci
 	
-
 
 
 	Ulamek(const Ulamek &other);
-		 //konstruktor kopiujący, jako parametr dostaje referencje do innego ułamka, który ma skopiować, ma do			step do prywatnych obiektow, bo jest zdefiniowany w tej klasie
-	
+		
 	~Ulamek();
 
 	string toString()const;
-						//const, bo nie zmienia obiektu;	
+						
 };
